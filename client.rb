b4cdd2e72d09e9b29e1d088d9e3f69b74fbc95fc
ishida-k <ishida-k@matsukei.co.jp>
2014-10-04 16:48:54 +0900

    クライアント側整理

--- client.rb
+++ client.rb
@@ -1,19 +1,28 @@
 require 'socket'
 require 'json'
 
-socket = TCPSocket.open('localhost', 8888)
+def seen_judge(data, line)
+  case data['seen'].to_i
+  when 1
+    return {name: line, seen: 2}
+  when 2
+    return {seen: 3, question_no: 1}
+  end
+end
+
+@socket = TCPSocket.open('localhost', 8888)
+send_data = {
+      name: '',
+      seen: 0,
+      question_no: "",
+      answer: ""
+  }
 
 STDIN.each_line do |line|
 
-  data = {
-      name: line,
-      seen: "1",
-      kaisuu: "1",
-      kaitou: "gehoge"
-  }
+  @socket.puts send_data.to_json
 
-  data = data.to_json
-  socket.puts(data)
-  puts socket.gets
+  recv_data = JSON.parse(@socket.gets)
 
+  send_data = seen_judge(recv_data, line)
 end
--- server.rb
+++ server.rb
@@ -16,23 +16,40 @@ loop do
 
     while s = client.gets
       res = JSON.parse(s)
-      puts res["message"]
-      puts res["seen"]
-      client.puts s
+
       case res["seen"].to_i
+        when 0
+          data = {
+            name: '',
+            seen: 1,
+            question_no: '',
+            answer: ''
+          }
+
+          client.puts data.to_json
         when 1
           #名前入力
           @name = res["name"]
+
+          data = {
+            name: @name,
+            seen: 2,
+            question_no: "",
+            answer: ""
+          }
+          client.puts data.to_json
         when 2
           #スタート画面
           @score = 0
+          res['answer'] = 'answer'
+          client.puts res.to_json
         when 3
           #出題
           @question = list.shuffle
-          @quesiton[res[:kaisuu].to_i]
+          @question[res[:question_no].to_i]
         when 4
           #判定
-          if res[:kaitou] == @quesiton[res[:kaisuu].to_i]
+          if res[:kaitou] == @question[res[:question_no].to_i]
             #正解の判定
           else
             #不正解の判定
