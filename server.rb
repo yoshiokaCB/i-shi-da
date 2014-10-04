9ddf5cdf76e3d0f5497148aa2ccc64c0d9ae8c46
tm21 <yoshioka@tm-21.com>
2014-10-04 14:27:51 +0900

    接続完了

--- server.rb
+++ server.rb
@@ -3,8 +3,7 @@ require 'socket'
 server = TCPServer.open(8888)
 loop do
   Thread.new(server.accept) do |client|
-    while client.gets
-      s = client.gets
+    while s = client.gets
       client.puts s
     end
   end
