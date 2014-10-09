5fa4900c5b1d52564089f6ecc6d46482f4a979b4
tm21 <yoshioka@tm-21.com>
2014-10-10 00:31:20 +0900

    includeを削除 クラスは相対パスで呼び出しをするように修正

--- server.rb
+++ server.rb
@@ -3,12 +3,11 @@ Bundler.require
 require 'socket'
 require 'json'
 require './lib/server/data_controller'
-include Server
 
 server = TCPServer.open(8888)
 loop do
   Thread.new(server.accept) do |client|
-    data = DataController.new
+    data = Server::DataController.new
     while s = client.gets
       client.puts data.create_data(JSON.parse(s)).to_json
     end
