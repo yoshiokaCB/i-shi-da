1f7eb7c1b01649d33fe095e4ec59e9e66302357b
ishida-k <ishida-k@matsukei.co.jp>
2014-10-10 21:13:35 +0900

    時間計測機能を追加

--- lib/client/controller.rb
+++ lib/client/controller.rb
@@ -17,13 +17,17 @@ module Client
       when "start"
         clear_console
 
+        @start_time = Time.now
+
         return {scene: "start" }
       when "question"
         return {scene: "question", answer: line, question_no: data["question_no"]}
       when "result"
+        @end_time = Time.now
+
         clear_console
 
-        return {scene: "result", answer: line, question_no: 10}
+        return {scene: "result", answer: line, question_no: 10, time: (@end_time - @start_time).truncate}
       end
     end
   end
