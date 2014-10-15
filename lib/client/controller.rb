2a1866c39956063060a26b76e431c3dc61cc1ccb
ishida-k <ishida-k@matsukei.co.jp>
2014-10-15 15:01:18 +0900

    各画面でメッセージを表示

--- lib/client/controller.rb
+++ lib/client/controller.rb
@@ -32,5 +32,33 @@ module Client
         return {scene: "retry", select: line}
       end
     end
+
+    def display_message(data)
+      if data['scene'] == 'name'
+        puts "=================="
+        puts "     タイトル     "
+        puts "=================="
+        puts "～説明～"
+        puts
+        puts "[名前を入力してください]"
+      elsif data['scene'] == 'start'
+        puts
+        puts "[スペースを送信でスタート]"
+      elsif data['scene'] == 'question' || data['scene'] == 'result'
+        puts
+        puts "[問#{data['question_no']}]"
+        puts data['question']
+        puts
+        puts "[入力]"
+      elsif data['scene'] == 'retry'
+        puts "[結果]"
+        puts
+        puts "リトライしますか？(y/n)"
+      elsif data['scene'] == 'end'
+        puts
+        puts '終了します'
+        exit
+      end
+    end
   end
 end
